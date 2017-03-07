9b38b990c19df2d8d81f2eabb548fe946d658860
jmleroux <jmleroux.pro@gmail.com>
2017-03-07 10:33:07 +0100

    TextCollection attribute type : template property as parameter

--- src/DataGrid/Extension/Formatter/Property/ProductValue/TextCollectionProperty.php
+++ src/DataGrid/Extension/Formatter/Property/ProductValue/TextCollectionProperty.php
@@ -10,15 +10,12 @@ use Symfony\Component\Translation\TranslatorInterface;
  * Formatter for Text Collection attribute type.
  * Works with a twig template (Resources/views/Property/text_collection.html.twig).
  *
- * @author JM Leroux <jean-marie.leroux@akeneo.com>
+ * @author    JM Leroux <jean-marie.leroux@akeneo.com>
  * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class TextCollectionProperty extends TwigProperty
 {
-    /** @var PresenterInterface */
-    protected $presenter;
-
     /**
      * {@inheritdoc}
      */
@@ -28,7 +25,8 @@ class TextCollectionProperty extends TwigProperty
 
         return $this->getTemplate()->render(
             [
-                'values' => $result,
+                'values'    => $result,
+                'attribute' => $value['attribute'],
             ]
         );
     }
