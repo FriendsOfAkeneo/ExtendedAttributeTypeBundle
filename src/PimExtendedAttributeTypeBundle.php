0f0bc17fe672b869d69f10c74d3ea2f3bccaaaed
jean-marie leroux <jmleroux.pro@gmail.com>
2017-05-10 15:53:11 +0200

    ElasticSearch compatibility (#26) (#27)

--- src/PimExtendedAttributeTypeBundle.php
+++ src/PimExtendedAttributeTypeBundle.php
@@ -2,6 +2,8 @@
 
 namespace Pim\Bundle\ExtendedAttributeTypeBundle;
 
+use Pim\Bundle\ElasticSearchBundle\Query\ProductQueryUtility;
+use Pim\Bundle\ExtendedAttributeTypeBundle\AttributeType\ExtendedAttributeTypes;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
@@ -11,4 +13,13 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class PimExtendedAttributeTypeBundle extends Bundle
 {
+    public function boot()
+    {
+        parent::boot();
+
+        $registeredBundles = $this->container->getParameter('kernel.bundles');
+        if (array_key_exists('PimElasticSearchBundle', $registeredBundles)) {
+            ProductQueryUtility::addTypeSuffix(ExtendedAttributeTypes::TEXT_COLLECTION, ProductQueryUtility::SUFFIX_TEXT);
+        }
+    }
 }
