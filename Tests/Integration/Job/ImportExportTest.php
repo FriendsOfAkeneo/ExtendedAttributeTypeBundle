34238921e72d52d79a31ab8da4b2665e75a70761
Mathias Métayer <mathias.metayer@akeneo.com>
2018-03-15 18:05:05 +0100

    Add tests

--- Tests/Integration/Job/ImportExportTest.php
+++ Tests/Integration/Job/ImportExportTest.php
@@ -1,12 +1,16 @@
 <?php
 
-namespace Pim\Bundle\ExtendedAttributeTypeBundle\Tests\Integration\ImportExport;
+namespace Pim\Bundle\ExtendedAttributeTypeBundle\Tests\Integration\Job;
 
 use Akeneo\Bundle\BatchBundle\Command\BatchCommand;
+use Akeneo\Component\Batch\Job\BatchStatus;
 use Akeneo\Component\Batch\Model\JobInstance;
 use Pim\Bundle\ExtendedAttributeTypeBundle\AttributeType\ExtendedAttributeTypes;
 use Pim\Bundle\ExtendedAttributeTypeBundle\Tests\Integration\AbstractTestCase;
 use Pim\Component\Catalog\AttributeTypes;
+use Pim\Component\Catalog\Model\ProductInterface;
+use Pim\Component\Catalog\Query\Filter\Operators;
+use Pim\Component\Catalog\Query\ProductQueryBuilderInterface;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
@@ -17,15 +21,19 @@ use Symfony\Component\Finder\Finder;
  * @copyright 2018 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class ExportTest extends AbstractTestCase
+class ImportExportTest extends AbstractTestCase
 {
+    private $resourcePath = __DIR__ . '/../../resources';
+
     public function testCsvExport()
     {
+        $exportFilePath = '/tmp/integration/export_products.csv';
+
         $this->getDataLoader()->createJobInstance(
             'csv_product_export',
             'export',
             [
-                'filePath' => '/tmp/integration/export_products.csv',
+                'filePath' => $exportFilePath,
                 'filters'  => [
                     'data'      => [],
                     'structure' => [
@@ -36,39 +44,6 @@ class ExportTest extends AbstractTestCase
                             'fr_FR',
                         ],
                     ],
-                ],
-            ]
-        );
-
-        $this->getDataLoader()->createAttribute(
-            [
-                'code'        => 'my_text_collection',
-                'type'        => ExtendedAttributeTypes::TEXT_COLLECTION,
-                'localizable' => true,
-            ]
-        );
-        $this->getDataLoader()->createAttribute(
-            [
-                'code' => 'name',
-                'type' => AttributeTypes::TEXT,
-            ]
-        );
-
-        $this->getDataLoader()->createFamily(
-            [
-                'code'       => 'first_family',
-                'attributes' => [
-                    'name',
-                ],
-            ]
-        );
-
-        $this->getDataLoader()->createFamily(
-            [
-                'code'       => 'second_family',
-                'attributes' => [
-                    'name',
-                    'my_text_collection',
                 ],
             ]
         );
@@ -142,19 +117,59 @@ class ExportTest extends AbstractTestCase
         $status = $this->launch('csv_product_export');
         $this->assertSame(BatchCommand::EXIT_SUCCESS_CODE, $status);
 
-        $this->assertFileExists('/tmp/integration/export_products.csv');
-        $this->assertFileEquals(__DIR__ . '/../../resources/products.csv', '/tmp/integration/export_products.csv');
+        $this->assertFileExists($exportFilePath);
+        $this->assertFileEquals($this->resourcePath . '/products.csv', $exportFilePath);
     }
 
+    public function testImportCreateProducts()
+    {
+        $this->getDataLoader()->createJobInstance(
+            'csv_product_import',
+            'import',
+            [
+                'filePath' => $this->resourcePath . '/products.csv',
+            ]
+        );
+
+        $status = $this->launch('csv_product_import');
+        $this->assertSame(BatchCommand::EXIT_SUCCESS_CODE, $status);
+
+        /** @var ProductQueryBuilderInterface $pqb */
+        $pqb = $this->get('pim_catalog.query.product_query_builder_factory')->create();
+        $products = $pqb->execute();
+
+        $this->assertCount(3, $products);
+        $secondSku = $pqb->addFilter('sku', Operators::EQUALS, 'second_sku')->execute()->current();
+
+        $this->assertInstanceOf(ProductInterface::class, $secondSku);
+        $this->assertContains('my_text_collection', $secondSku->getUsedAttributeCodes());
+        $this->assertEquals(['bar', 'baz', 'foo'], $secondSku->getValue('my_text_collection', 'es_ES')->getData());
+
+
+        $pqb = $this->get('pim_catalog.query.product_query_builder_factory')->create();
+        $thirdSku = $pqb->addFilter('sku', Operators::EQUALS, 'third_sku')->execute()->current();
+        $this->assertInstanceOf(ProductInterface::class, $thirdSku);
+        $this->assertNotContains('my_text_collection', $thirdSku->getUsedAttributeCodes());
+
+        $myTextCollection = $this->get('pim_catalog.repository.attribute')->findOneByIdentifier('my_text_collection');
+        $this->assertTrue($thirdSku->hasAttributeInFamily($myTextCollection));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function setUp()
     {
         parent::setUp();
-        $this->getDataLoader()->activateLocales(['fr_FR', 'es_ES']);
+        $this->loadData();
         if (!file_exists('/tmp/integration')) {
             mkdir('/tmp/integration');
         }
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function tearDown()
     {
         $finder = new Finder();
@@ -163,5 +178,45 @@ class ExportTest extends AbstractTestCase
         foreach ($files as $file) {
             unlink($file);
         }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    private function loadData()
+    {
+        $this->getDataLoader()->activateLocales(['fr_FR', 'es_ES']);
+        $this->getDataLoader()->createAttribute(
+            [
+                'code'        => 'my_text_collection',
+                'type'        => ExtendedAttributeTypes::TEXT_COLLECTION,
+                'localizable' => true,
+            ]
+        );
+        $this->getDataLoader()->createAttribute(
+            [
+                'code' => 'name',
+                'type' => AttributeTypes::TEXT,
+            ]
+        );
+
+        $this->getDataLoader()->createFamily(
+            [
+                'code'       => 'first_family',
+                'attributes' => [
+                    'name',
+                ],
+            ]
+        );
+
+        $this->getDataLoader()->createFamily(
+            [
+                'code'       => 'second_family',
+                'attributes' => [
+                    'name',
+                    'my_text_collection',
+                ],
+            ]
+        );
     }
 }
